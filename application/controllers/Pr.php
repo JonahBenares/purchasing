d0e1bd5be4e81aec7f4a4485ae82cc8dda1dfb46
stephinesev <stephineseverino.cenpri@gmail.com>
2019-06-26 10:58:45 +0800

    Import Function

--- application/controllers/Pr.php
+++ application/controllers/Pr.php
@@ -46,7 +46,7 @@ class Pr extends CI_Controller {
                 $error_ext++;
             } 
             else {
-                 $filename1='Purcahse Request.'.$ext1;
+                 $filename1='Purchase Request.'.$ext1;
                 if(move_uploaded_file($_FILES["excelfile_pr"]['tmp_name'], $dest.'/'.$filename1)){
                     $this->readExcel_pr();
                 }   
@@ -65,24 +65,57 @@ class Pr extends CI_Controller {
         } catch(Exception $e) {
             die('Error loading file"'.pathinfo($inputFileName,PATHINFO_BASENAME).'": '.$e->getMessage());
         }
-
-
-
-        $highestRow = $objPHPExcel->getActiveSheet()->getHighestRow(); 
-        for($x=14;$x<=$highestRow;$x++){
-            $qty = trim($objPHPExcel->getActiveSheet()->getCell('B'.$x)->getValue());
-            $uom = trim($objPHPExcel->getActiveSheet()->getCell('C'.$x)->getValue());
-            $part_no = trim($objPHPExcel->getActiveSheet()->getCell('D'.$x)->getValue());
-            $description = trim($objPHPExcel->getActiveSheet()->getCell('E'.$x)->getValue());
-            $date_needed = trim($objPHPExcel->getActiveSheet()->getCell('J'.$x)->getValue());
-            $data_items = array(
-                'item_id'=>$itemid,
-                'catalog_no'=>$catalog,
-                'quantity'=>$qty
-            );
-            $this->super_model->insert_into("supplier_items", $data_items);
+        $head_rows = $this->super_model->count_rows("pr_head");
+        if($head_rows==0){
+            $pr_id=1;
+        } else {
+            $maxid=$this->super_model->get_max("pr_head", "pr_id");
+            $pr_id=$maxid+1;
         }
-        echo "<script>alert('Successfully Uploaded!'); window.location = 'purchase_request';</script>";
+
+        $pr = trim($objPHPExcel->getActiveSheet()->getCell('C7')->getValue());
+        $date_prepared = trim($objPHPExcel->getActiveSheet()->getCell('C8')->getValue());
+        /*$date_issued = trim($objPHPExcel->getActiveSheet()->getCell('C9')->getValue());*/
+        $pr_no = trim($objPHPExcel->getActiveSheet()->getCell('C10')->getValue());
+        $purpose = trim($objPHPExcel->getActiveSheet()->getCell('C11')->getValue());
+        $enduse = trim($objPHPExcel->getActiveSheet()->getCell('C12')->getValue());
+        $department = trim($objPHPExcel->getActiveSheet()->getCell('I7')->getValue());
+        $requestor = trim($objPHPExcel->getActiveSheet()->getCell('I8')->getValue());
+        $urgency_no = trim($objPHPExcel->getActiveSheet()->getCell('I9')->getValue());
+
+        $data_head = array(
+            'user_pr_no'=>$pr_no,
+            'purchase_request'=>$pr,
+            'date_prepared'=>$date_prepared,
+            'enduse'=>$enduse,
+            'purpose'=>$purpose,
+            'department'=>$department,
+            'requestor'=>$requestor,
+            'urgency'=>$urgency_no,
+            'date_imported'=>date('Y-m-d H:i:s'),
+            'imported_by'=>$_SESSION['user_id'],
+        );
+
+        if($this->super_model->insert_into("pr_head", $data_head)){
+            $highestRow = $objPHPExcel->getActiveSheet()->getHighestRow(); 
+            for($x=14;$x<=$highestRow;$x++){
+                $qty = trim($objPHPExcel->getActiveSheet()->getCell('B'.$x)->getValue());
+                $uom = trim($objPHPExcel->getActiveSheet()->getCell('C'.$x)->getValue());
+                $part_no = trim($objPHPExcel->getActiveSheet()->getCell('D'.$x)->getValue());
+                $description = trim($objPHPExcel->getActiveSheet()->getCell('E'.$x)->getValue());
+                $date_needed = trim($objPHPExcel->getActiveSheet()->getCell('J'.$x)->getValue());
+                $data_items = array(
+                    'pr_id'=>$pr_id,
+                    'item_description'=>$description,
+                    'uom'=>$uom,
+                    'quantity'=>$qty,
+                    'part_no'=>$part_no,
+                    'date_needed'=>$date_needed,
+                );
+                $this->super_model->insert_into("pr_details", $data_items);
+            }
+        }
+        echo "<script>alert('Successfully Uploaded!'); window.location = 'purchase_request/$pr_id';</script>";
     }
 
     public function purchase_request(){  
