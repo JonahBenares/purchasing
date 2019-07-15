4162802c0c07110a661e59508b21c1df1af47d00
Jonah Benares <jonahbenares.cenpri@gmail.com>
2019-07-15 10:39:38 +0800

    rfd list

--- application/controllers/Rfdis.php
+++ application/controllers/Rfdis.php
@@ -41,10 +41,16 @@ class Rfdis extends CI_Controller {
 
 	}
 
+     public function get_name($column, $table, $where){
+        $col = $this->super_model->select_column_custom_where($table, $column, $where);
+        return $col;
+    }
+
 	public function rfdis_list(){	
+        $data['head']= $this->super_model->select_all_order_by("rfd", "rfd_date", "DESC");
         $this->load->view('template/header');
         $this->load->view('template/navbar');
-        $this->load->view('rfdis/rfdis_list');
+        $this->load->view('rfdis/rfdis_list',$data);
         $this->load->view('template/footer');
     }
 
