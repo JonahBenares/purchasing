f56329ac646cd96a70c8408e22852616dddc7f2a
ProjectPixel <jasonflor.cenpri@gmail.com>
2019-08-06 15:29:15 +0800

    Aug 06 2019 PO Revised inland freight

---
+++ application/views/po/purchase_order_rev.php
@@ -0,0 +1,346 @@
+  	<script src="<?php echo base_url(); ?>assets/js/po.js"></script> 
+  	<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Procurement System</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- favicon
+    		============================================ -->
+        <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url(); ?>assets/img/message/logo4.ico">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.css">
+	    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/mixins.css">
+	    <script src="<?php echo base_url(); ?>assets/js/all-scripts.js"></script> 
+	</head>
+
+  	<style type="text/css">
+        html, body{
+            background: #2d2c2c!important;
+            font-size:12px!important;
+        }
+        .pad{
+        	padding:0px 250px 0px 250px
+        }
+        .table-bordered>tbody>tr>td, 
+        .table-bordered>tbody>tr>th, 
+        .table-bordered>tfoot>tr>td, 
+        .table-bordered>tfoot>tr>th, 
+        .table-bordered>thead>tr>td, 
+        .table-bordered>thead>tr>th,
+        .all-border
+        {
+		    border: 1px solid #000!important;
+		}
+		.f13{
+			font-size:13px!important;
+		}
+		.bor-btm{
+			border-bottom: 1px solid #000;
+		}
+		.bor-right{
+			border-right: 1px solid #000;
+		}
+		.sel-des{
+			border: 0px!important;
+		}
+		.yellow-back{
+			background-image: url('<?php echo base_url(); ?>assets/img/yellow.png')!important;
+		}
+		.green-back{
+			background-image: url('<?php echo base_url(); ?>assets/img/green.png')!important;
+		}
+		@media print{
+			.pad{
+        	padding:0px 0px 0px 0px
+        	}
+			#prnt_btn,#item-btn,#pr-btn{
+				display: none;
+			}
+			.emphasis{
+				border: 0px solid #fff!important;
+			}
+			html, body{
+            background: #fff!important;
+            font-size:12px!important;
+        	}
+        	.yellow-back{
+			background-image: url('<?php echo base_url(); ?>assets/img/yellow.png')!important;
+			}
+			.green-back{
+				background-image: url('<?php echo base_url(); ?>assets/img/green.png')!important;
+			}
+		}
+		.text-white{
+			color: #fff;
+		}
+		.select-des{			
+		    -webkit-appearance: none;
+		    border: 0px;
+		}
+	
+		.emphasis{
+			border-bottom: 2px solid red;
+		}
+		.text-red{
+			color: red;
+		}
+		.nomarg{
+			margin: 0px 2px 0px 2px;
+		}
+    </style>
+
+    <div  class="pad">
+    	<form method='POST' action='<?php echo $url; ?>'>  
+    		<div  id="prnt_btn">
+	    		<center>
+			    	<div class="abtn-group">
+						<a href="javascript:history.go(-1)" class="btn btn-success btn-md p-l-50 p-r-50"><span class="fa fa-arrow-left"></span> Back</a>
+						<a  onclick="printPage()" class="btn btn-warning btn-md p-l-50 p-r-50"><span class="fa fa-print"></span> Print</a>
+							<input type='submit' class="btn btn-primary btn-md p-l-50 p-r-50" value="Save Revision">	
+							<input type='submit' class="btn btn-primary btn-md p-l-50 p-r-50" value="Save">	
+					</div>
+					<p class="text-white">Instructions: When printing PURCHASE ORDER make sure the following options are set correctly -- <u>Browser</u>: Chrome, <u>Layout</u>: Portrait, <u>Paper Size</u>: A4, <u>Margin</u> : Default, <u>Scale</u>: 100</p>
+				</center>
+			</div>
+	    	<div style="background: #fff;">    		  			
+		    	<table class="table-borsdered" width="100%" style="border:2px solid #000">
+		    		<tr>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    			<td width="5%"><br></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="6" align="center"><img width="150px" src="<?php echo base_url(); ?>assets/img/logo_cenpri.png"></td>
+		    			<td colspan="14"><h4 style="margin: 0px"><b>CENTRAL NEGROS POWER RELIABILITY, INC.</b></h4></td>
+		    		</tr>
+		    		<tr><td class="f13" colspan="20" align="center">Office: 88 Corner Rizal-Mabini Sts., Bacolod City</td></tr>
+		    		<tr><td class="f13" colspan="20" align="center">Tel. No.: (034) 435-1932/476-7382</td></tr>
+		    		<tr><td class="f13" colspan="20" align="center">Telefax: (034) 435-1932</td></tr>
+		    		<tr><td class="f13" colspan="20" align="center">Plant Site: Purok San Jose, Barangay Calumangan, Bago City</td></tr>
+		    		<tr><td colspan="20" align="center"><h4 class="m-b-0"><b>PURCHASE ORDER</b></h4><small class="text-red">R E V I S E D</small></td></tr>
+		    		<tr><td class="f13" colspan="20" align="center"><br></td></tr>
+		    		<tr>
+		    			<td colspan="3"><h6 class="nomarg"><b>Date</b></h6></td>
+		    			<td colspan="12"><h6 class="nomarg"><b></b></h6></td>
+		    			<td colspan="5"><h6 class="nomarg"><b>P.O. No.: </b></h6></td>
+		    		</tr>	
+		    		<tr>
+		    			<td colspan="3"><h6 class="nomarg"><b>Supplier:</b></h6></td>
+		    			<td colspan="12"><h6 class="nomarg bor-btm"><b></b></h6></td>
+		    			<td colspan="5"><h6 class="nomarg"><b></b></h6></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="3"><h6 class="nomarg"><b>Address:</b></h6></td>
+		    			<td colspan="12"><h6 class="nomarg bor-btm"><b></b></h6></td>
+		    			<td colspan="5"><h6 class="nomarg"><b></b></h6></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="3"><h6 class="nomarg"><b>Contact Person:</b></h6></td>
+		    			<td colspan="12"><h6 class="nomarg bor-btm"><b></b></h6></td>
+		    			<td colspan="5"><h6 class="nomarg"><b></b></h6></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="3"><h6 class="nomarg"><b>Telephone #:</b></h6></td>
+		    			<td colspan="12"><h6 class="nomarg bor-btm"><b></b></h6></td>
+		    			<td colspan="5"><h6 class="nomarg"><b></b></h6></td>
+		    		</tr>
+					<tr>
+		    			<td colspan="" class="all-border" align="center"><b>#</b></td>
+		    			<td colspan="2" class="all-border" align="center"><b>Old Qty</b></td>
+		    			<td colspan="2" class="all-border yellow-back" align="center"><b>New Qty</b></td>
+		    			<td colspan="" class="all-border" align="center"><b>Unit</b></td>
+		    			<td colspan="8" class="all-border" align="center"><b>Description</b></td>
+		    			<td colspan="2" class="all-border" align="center"><b>OLD U/P</b></td>
+		    			<td colspan="2" class="all-border yellow-back" align="center"><b>NEW U/P</b></td>
+		    			<td colspan="2" class="all-border" align="center"></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="" class="bor-right" align="center"><b></b></td>
+		    			<td colspan="2" class="bor-right" align="center"><b><input type='number' name='quantity' id='quantity' class='quantity' value='' style='width:100%; color:red' onblur='changePrice()' onkeypress="return isNumberKey(this, event)"></b></td>
+		    			<td colspan="2" class="bor-right yellow-back" align="center"><b><input type='number' name='quantity' id='quantity' class='quantity' value='' style='width:100%; color:red' onblur='changePrice()' onkeypress="return isNumberKey(this, event)"></b></td>
+		    			<td colspan="" class="bor-right" align="center"><b></b></td>
+		    			<td colspan="8" class="bor-right" align="left"><b class="nomarg"></b></td>
+		    			<td colspan="2" class="bor-right" align="center"><b><input type='text' name='price' id='price' value='' onblur='changePrice()' onkeypress="return isNumberKey(this, event)" style='color:red; width:100px' ></b></td>
+		    			<td colspan="2" class="bor-right yellow-back" align="center"><b><input type='text' name='price' id='price' value='' onblur='changePrice()' onkeypress="return isNumberKey(this, event)" style='color:red; width:100px' ></b></td>
+		    			<td colspan="2" class="bor-right" align="right"><b class="nomarg"><input type='text' name='tprice' id='tprice' class='tprice' value="" style='text-align:right;' readonly></b></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="" class=" bor-right" align="center"></td>
+		    			<td colspan="2" class=" bor-right" align="center"></td>
+		    			<td colspan="2" class=" bor-right yellow-back" align="center"></td>
+		    			<td colspan="" class=" bor-right" align="center"></td>
+		    			<td colspan="8" class=" bor-right" align="center"></td>
+		    			<td colspan="2" class=" bor-right" align="center"><br></td>
+		    			<td colspan="2" class=" bor-right yellow-back" align="center"><br></td>
+		    			<td colspan="2" class=" bor-right" align="center"></td>
+		    		</tr>
+
+
+		    		<tr>
+		    			<td colspan="" class="bor-btm bor-right" align="center"></td>
+		    			<td colspan="2" class="bor-btm bor-right" align="center"></td>
+		    			<td colspan="2" class="bor-btm bor-right yellow-back" align="center"></td>
+		    			<td colspan="" class="bor-btm bor-right" align="center"></td>
+		    			<td colspan="8" class="bor-btm bor-right" align="left">
+		    				<p class="nomarg">
+		    					Enduse: <br>
+		    					Purpose: <br>
+		    					Requestor: <br>
+		    					PR no.: <br>
+		    				</p>
+		    				<br>
+		    			</td>
+		    			<td colspan="2" class="bor-btm bor-right" align="center"><br></td>
+		    			<td colspan="2" class="bor-btm bor-right yellow-back" align="center"><br></td>
+		    			<td colspan="2" class="bor-btm bor-right" align="center"></td>
+		    		</tr>		    		
+		    		<tr>
+		    			<td colspan="18" class="all-border" align="right"><b class="nomarg">GRAND TOTAL</b></td>
+					    <td colspan="2" class="all-border" align="right"><b class="nomarg"><span class="pull-left">₱</span><span id='grandtotal'></span></b></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="20">
+		    				<i></i>
+		    			</td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="20" style="padding: 10px!important">
+		    				<button type="button" class="btn btn-primary btn-xs " data-toggle="modal" data-target="#exampleModal">
+							 Add Terms & Conditions
+							</button>
+		    				<br>Terms & Conditions:<br>
+		    				1. Price is inclusive of taxes.<br>
+		    				2. PO No. must appear on all copies of Invoices, Delivery Receipt & Correspondences submitted.<br>
+		    				3. Sub-standard items shall be returned to supplier @ no cost to CENPRI.<br>
+		    				4. Payment term: PAYEMENT TERMS HERE<br>
+		    				5. Delivery Time: <br>
+		    				6. In-land Freight: Included
+		    				<br>
+		    				<br>
+		    				<button type="button" class="btn btn-primary btn-xs " data-toggle="modal" data-target="#otherins">
+							 Add Other Instruction
+							</button>
+
+		    			</td>
+		    		</tr>
+		    		<tr><td colspan="20"><br></td></tr>
+		    		<tr>
+		    			<td colspan="2"></td>
+		    			<td colspan="7"><b>Prepared by:</b></td>
+		    			<td colspan="2"></td>
+		    			<td colspan="7"><b>Approved by:</b></td>
+		    			<td colspan="2"></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="2"></td>
+		    			<td colspan="7" class="bor-btm"><b><br></b></td>
+		    			<td colspan="2"></td>
+		    			<td colspan="7" class="bor-btm"><b><br></b></td>
+		    			<td colspan="2"></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="2"></td>
+		    			<td colspan="7"><b></b></td>
+		    			<td colspan="2"></td>
+		    			<td colspan="7"><b>
+		    			<select name='approved' class="select-des emphasis" style="width: 100%" required>
+			    			<option value=''>-Select-</option>
+		    			</select></b></td>
+		    			<td colspan="2"></td>
+		    		</tr>
+		    		<tr><td colspan="20"><br></td></tr>
+		    		<tr>
+		    			<td colspan="4"></td>
+		    			<td colspan="2"><b>Conforme:</b></td>
+		    			<td colspan="8" class="bor-btm"><b></b></td>
+		    			<td colspan="6"></td>
+		    		</tr>
+		    		<tr>
+		    			<td colspan="4"></td>
+		    			<td colspan="2"><b></b></td>
+		    			<td colspan="8" align="center"><b>Supplier's Signature Over Printed Name</b></td>
+		    			<td colspan="6"></td>
+		    		</tr>
+		    		<tr><td colspan="20"><br></td></tr>
+		    		<tr><td colspan="20"><br></td></tr>
+		    	</table>	    
+	    	</div>
+	    	<input type='hidden' name='po_id' value='<?php echo $po_id; ?>'>
+    	</form>
+    	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Add Terms & Conditions
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</h5>
+						
+					</div>
+					<form method="POST" action="<?php echo base_url(); ?>po/add_tc">
+						<div class="modal-body">
+							<div class="form-group">
+								Terms & Conditions:
+								<input type="text" class="form-control" name="tc_desc" autocomplete="off">
+							</div>
+						</div>
+						<input type='hidden' name='po_id' value='<?php echo $po_id; ?>'>
+						<div class="modal-footer">
+							<input type="submit" class="btn btn-primary btn-block" value="Save changes">
+						</div>
+
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="modal fade" id="otherins" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Add Other Instructions
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+							</button>
+						</h5>						
+					</div>
+					<form method="POST" action="<?php echo base_url(); ?>po/">
+						<div class="modal-body">
+							<div class="form-group">
+								Other Instructions:
+								<textarea class="form-control" rows="5"></textarea>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<input type="submit" class="btn btn-primary btn-block" value="Save changes">
+						</div>
+
+					</form>
+				</div>
+			</div>
+		</div>
+    </div>
+    <script type="text/javascript">
+    	function printPage() {
+		  window.print();
+		}
+    </script>
