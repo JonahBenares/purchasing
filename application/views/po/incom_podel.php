d0b855304a14a798f996d957454e6b590cb5009c
ProjectPixel <jasonflor.cenpri@gmail.com>
2019-08-28 13:45:59 +0800

    Incomplete PO Delivery

---
+++ application/views/po/incom_podel.php
@@ -0,0 +1,87 @@
+    <script src="<?php echo base_url(); ?>assets/js/po.js"></script> 
+    <div class="breadcome-area mg-b-30 small-dn">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcome-list map-mg-t-40-gl shadow-reset">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                <div class="breadcome-heading">
+                                    <form role="search" class="">
+                                        <input type="text" placeholder="Search..." class="form-control">
+                                        <a href=""><i class="fa fa-search"></i></a>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                <ul class="breadcome-menu">
+                                    <li><a href="<?php echo base_url(); ?>">Home</a> <span class="bread-slash">/</span>
+                                    </li>
+                                    <li><a href="<?php echo base_url(); ?>/po/po_list">PO list</a> <span class="bread-slash">/</span>
+                                    </li>
+                                    <li><span class="bread-blod">Incomplete PO Delivery List</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="admin-dashone-data-table-area">
+        <div class="container-fluid">
+            <form name="myform" action="<?php echo base_url(); ?>index.php/aoq/add_aoq" method="POST">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="sparkline8-list shadow-reset">
+                            <div class="sparkline8-hd" style="background: #ffbb33">
+                                <div class="main-sparkline8-hd">
+                                    <h1 class="text-white">Incomplete PO Delivery</h1>
+                                    <small class="text-white">PURCHASE ORDER</small>
+                                    <div class="sparkline8-outline-icon">
+                                        <h2><span class="fa fa-adjust"></span></h2>
+                                    </div>
+                                </div>
+                            </div>                       
+                            <div class="sparkline8-graph">
+                                <div class="datatable-dashv1-list custom-datatable-overright">
+                                    <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true" data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar">
+                                        <thead>
+                                            <tr>
+                                                <th>PO Date</th>
+                                                <th>PO #</th>
+                                                <th>Supplier</th>
+                                                <th>PR #</th>
+                                                <th>Status</th>
+                                                <th>Mode of Purchase</th>
+                                                <th><center><span class="fa fa-bars"></span></center></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                
+                                                <td><a class="btn-link txt-primary" onclick="viewHistory('')"></a></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td>
+                                                    <center>                                                        
+                                                        <a href="" class="btn btn-custon-three btn-warning btn-xs " title='Dr'>
+                                                            <span class="fa fa-check"></span>
+                                                        </a>
+                                                    </center>
+                                                </td>
+                                            </tr>                                 
+                                        </tbody>
+                                    </table>
+                                </div>                           
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
