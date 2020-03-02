ddf6d482d8fbc45ba077cefe4c8b571749350ca2
unknown <jonahbenares.cenpri@gmail.com>
2020-03-02 11:23:11 +0800

    remove required

--- application/views/pod/po_direct.php
+++ application/views/pod/po_direct.php
@@ -533,7 +533,7 @@
 		    			<td colspan="1"></td>
 		    			<td colspan="4" class=""><b>
 			    			<?php if($saved==0){ ?>
-			    			<select name='checked' class="select-des emphasis" style="width: 100%" required>
+			    			<select name='checked' class="select-des emphasis" style="width: 100%" >
 				    			<option value=''>-Select-</option>
 				    			<?php foreach($employee AS $emp){ ?>
 				    			<option value='<?php echo $emp->employee_id; ?>'><?php echo $emp->employee_name; ?></option>
@@ -544,7 +544,7 @@
 			    			<td colspan="1"></td>
 		    			<td colspan="4" class=""><b>
 			    			<?php if($saved==0){ ?>
-			    			<select name='recommended' class="select-des emphasis" style="width: 100%" required>
+			    			<select name='recommended' class="select-des emphasis" style="width: 100%" >
 				    			<option value=''>-Select-</option>
 				    			<?php foreach($employee AS $emp){ ?>
 				    			<option value='<?php echo $emp->employee_id; ?>'><?php echo $emp->employee_name; ?></option>
