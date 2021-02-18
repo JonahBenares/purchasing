4ed2ff1e9dc1eeabed1cc69d5bc982bb8c22b95a
henneall <hentanan53@gmail.com>
2021-02-18 14:15:39 +0800

    files

--- assets/js/rfdis.js
+++
@@ -1,3 +0,0 @@
-function additemrfd(baseurl,rfdid,supplier_id) {
-    window.open(baseurl+"index.php/rfdis/additemrfd/"+rfdid+"/"+supplier_id, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=45,left=75,width=1200,height=600");
-}
