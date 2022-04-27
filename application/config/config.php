16d605c71be5ac398052762b0081334fd766b91f
hennetanan <hentanan.cenpri>
2022-04-27 12:40:25 +0800

    latest db

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://cenpri-server/purchasing';
+$config['base_url'] = 'http://localhost/purchasing';
 
 /*	
 |--------------------------------------------------------------------------
